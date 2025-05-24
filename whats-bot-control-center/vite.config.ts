24d574094e778149ae792fb8fdbb4a43d0d43b9e
Nathan <nathanalfa00@gmail.com>
2025-05-24 18:26:03 -0300

    chore(front): remover Dockerfile e usar buildpacks padrão

--- whats-bot-control-center/vite.config.ts
+++ whats-bot-control-center/vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
-  root: '.',
-  publicDir: 'public',
+  // instrui o Vite onde está o index.html
+  root: 'public',
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'index.html'
-    }
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      // agora "@/" = src/
+      '@': resolve(__dirname, 'src'),
+    },
   },
   plugins: [react()],
 })
